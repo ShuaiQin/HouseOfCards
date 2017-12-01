453fd4b54d64a001fe3f1d4c729ac368446d6785
ShuaiQin <qinshuai@sjtu.edu.cn>
2017-12-01 12:38:28 -0600

    add create-pr and resolve pr

--- webapp/handlers/CreatePage.py
+++ webapp/handlers/CreatePage.py
@@ -1,8 +1,9 @@
 from google.appengine.api import users
+from google.appengine.api import urlfetch
 import webapp2
 
 import config.config as cfg
-
+import json, urllib
 
 class CreatePage(webapp2.RequestHandler):
     def get(self):
@@ -19,3 +20,30 @@ class CreatePage(webapp2.RequestHandler):
             self.response.write(template.render(template_value))
         else:
             self.redirect("/login")
+
+    def post(self):
+        current_user = users.get_current_user().email()
+        name = self.request.get('name')
+        cover_url = self.request.get('cover_url')
+        category = self.request.get('category')
+
+        form_fields = {
+            'user_id': current_user,
+            'house_name': name,
+            'cover_url': cover_url,
+            'category': category
+        }
+        print form_fields
+        form_data = urllib.urlencode(form_fields)
+        print form_data
+        rpc = urlfetch.create_rpc()
+        url = cfg.SERVICE_URL + "/service-createhouse?" + form_data
+        print url
+        response = urlfetch.make_fetch_call(rpc, url)
+        result = rpc.get_result()
+        print result.content
+        data = json.loads(result.content)
+        if data['status']:
+            self.redirect('/house/' + name)
+        else:
+            self.redirect('/error')
--- webapp/handlers/HousePage.py
+++ webapp/handlers/HousePage.py
@@ -16,11 +16,17 @@ class HousePage(webapp2.RequestHandler):
             template_value['logout_url'] = cfg.LOG_OUT_URL
             template_value['sign'] = True
             template_value['house'] = name
-            data = self.rpc(user.nickname(), name)
-            template_value['cards'] = data['card_list']
-            template_value['is_owned'] = data['is_owned']
-            template_value['is_subed'] = data['is_subed']
-            template_value['house_info'] = data['house_info']
+
+            data = self.rpc(user.email(), name)
+            if data:
+                template_value['cards'] = data['card_list']
+                template_value['is_owned'] = data['is_owned']
+                template_value['is_subed'] = data['is_subed']
+                template_value['house_info'] = data['house_info']
+
+            data = self.pr_rpc(name)
+            if data:
+                template_value['prs'] = data
 
             template = cfg.JINJA_ENVIRONMENT.get_template("house.html")
             self.response.write(template.render(template_value))
@@ -29,12 +35,25 @@ class HousePage(webapp2.RequestHandler):
 
     def rpc(self, user_id, house_name):
         rpc = urlfetch.create_rpc()
-        request = {}
-        request['user_id'] = user_id
-        request['house_name'] = house_name
+        request = {
+            'user_id': user_id,
+            'house_name': house_name
+        }
         url = cfg.SERVICE_URL + "/service-viewsinglehouse?" + urllib.urlencode(request)
         print url
         urlfetch.make_fetch_call(rpc, url)
         response = rpc.get_result()
         data = json.loads(response.content)
         return data
+
+    def pr_rpc(self, house_name):
+        rpc = urlfetch.create_rpc()
+        request = {
+            'house_name': house_name
+        }
+        url = cfg.SERVICE_URL + "/service-showpullrequest?" + urllib.urlencode(request)
+        print url
+        urlfetch.make_fetch_call(rpc, url)
+        response = rpc.get_result()
+        data = json.loads(response.content)
+        return data["pull_request_list"]
